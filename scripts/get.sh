fd4a3b7ffec87e2e8b3df7eddcf1c5a058f6c20e
Philipp Haussleiter <philipp@haussleiter.de>
2018-04-19 18:56:48 +0200

    adding Dockerfile

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "accessing:"
-echo "curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json"
 
-curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json \
--H "Private-Token: $PRIVATE_ACCESS_TOKEN" \
--H "X-Requested-With: XMLHttpRequest" \
--H "Connection: keep-alive" \
--H "X-Requested-With: XMLHttpRequest" \
--H "Accept: application/json, text/plain, */*" --compressed
+#curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json \
+#-H "Private-Token: $PRIVATE_ACCESS_TOKEN" \
+#-H "X-Requested-With: XMLHttpRequest" \
+#-H "Connection: keep-alive" \
+#-H "X-Requested-With: XMLHttpRequest" \
+#-H "Accept: application/json, text/plain, */*" --compressed
+
+
+curl '$CI_PROJECT_URL/container_registry.json' \
+-u $CI_REGISTRY_USER:$CI_REGISTRY_PASSWORD \
+-H 'accept: application/json, text/plain, */*'

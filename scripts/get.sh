1541cf2dd48aef3af31535bea84591751e5d5afa
Philipp Haussleiter <philipp@haussleiter.de>
2018-04-19 18:22:50 +0200

    script update

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json \
--H "JOB-TOKEN: $CI_JOB_TOKEN" \
+-H "Private-Token: $CI_JOB_TOKEN" \
 -H "X-Requested-With: XMLHttpRequest" \
 -H "Connection: keep-alive" \
 -H "X-Requested-With: XMLHttpRequest" \

6e8200319168129bef265207c1dba00a66ebae95
Philipp Haussleiter <philipp@haussleiter.de>
2018-04-19 18:18:46 +0200

    script update

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json -u gitlab-ci-token:$CI_JOB_TOKEN \
--H 'X-Requested-With: XMLHttpRequest' \
--H 'Connection: keep-alive' \
--H 'X-Requested-With: XMLHttpRequest' \
--H 'Accept: application/json, text/plain, */*' --compressed
+curl $CI_PROJECT_URL/registry/repository/$CI_PROJECT_ID/tags?format=json \
+-H "JOB-TOKEN: $CI_JOB_TOKEN" \
+-H "X-Requested-With: XMLHttpRequest" \
+-H "Connection: keep-alive" \
+-H "X-Requested-With: XMLHttpRequest" \
+-H "Accept: application/json, text/plain, */*" --compressed

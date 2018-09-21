daef3e92454b68081a4e97e3fb499d60c3c97f71
Philipp Haussleiter <philipp@haussleiter.de>
2018-09-21 15:19:01 +0200

    maybe not producing an error?

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,8 @@ func initConfig() {
 	_, hasToken := os.LookupEnv("PRIVATE_ACCESS_TOKEN")
 	_, hasURL := os.LookupEnv("CI_PROJECT_URL")
 	if !hasToken || !hasURL {
-		log.Fatal("You need to set 'CI_PROJECT_URL' and 'PRIVATE_ACCESS_TOKEN'")
+		log.Println("You need to set 'CI_PROJECT_URL' and 'PRIVATE_ACCESS_TOKEN'")
+		os.Exit(0)
 	}
 
 	viper.Set("Token", os.Getenv("PRIVATE_ACCESS_TOKEN"))

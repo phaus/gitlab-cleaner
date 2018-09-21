bc48bf583caddf35360e9f2a79f9255aa2092d50
Philipp Haussleiter <philipp@haussleiter.de>
2018-09-21 16:00:31 +0200

    optimize container call

--- cleaner-entrypoint.sh
+++ cleaner-entrypoint.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-echo "Test!"
+printf 'before %s\n' "$@"
+
+which sh
 which cleaner
 
 if [[ $# -eq 0 ]] ; then
+    echo "exit"
     cleaner --help
     exit 0
 fi
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
+    echo "-"
     set -- cleaner "$@"
 fi
 
-printf '%s\n' "$@"
+printf 'after %s\n' "$@"
 
 exec "$@"

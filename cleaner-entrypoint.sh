3a7a8d6f8a6078d952b7ca63e8c5ce852f6d4540
Philipp Haussleiter <philipp@haussleiter.de>
2018-09-22 01:26:24 +0200

    fixing start-script

--- cleaner-entrypoint.sh
+++ cleaner-entrypoint.sh
@@ -2,24 +2,10 @@
 
 set -e
 
-printf '%s\n' "$@"
-
 if [[ $# -eq 0 ]] ; then
     cleaner --help
     exit 0
 fi
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-    set -- cleaner "$@"
-fi
-
-for arg 
-do
-    shift
-    [ "$arg" = "sh" -o "$arg" = "-c" ] && continue
-    set -- "$@" "$arg"
-done
 
 printf '%s\n' "$@"
 
